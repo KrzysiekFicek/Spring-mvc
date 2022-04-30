3ff6a0500b1a201ca2d9eb2ac8e52cd1be74759a
Krzysztof Ficek <krzysztof.ficek88@gmail.com>
2022-04-30 15:58:24 +0200

    Przekazywanie parametrów update RandomController

--- src/main/java/pl/coderslab/RandomController.java
+++ src/main/java/pl/coderslab/RandomController.java
@@ -2,6 +2,7 @@ package pl.coderslab;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
@@ -9,10 +10,27 @@ import java.util.Random;
 @Controller
 public class RandomController {
 
+    private final  Random random = new Random();
+
+    @GetMapping(value = "/random/{min}/{max}", produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String showRandomWithMax(@PathVariable int max, @PathVariable int min) {
+        int randomNumber = random.nextInt(max - min + 1) + min;
+        return "Użytkownik podał wartość: " + min + " i  " + max + ". Wylosowano liczbę: " + randomNumber;
+    }
+
+/* przekazywanie parametrów: ex 1
+    @GetMapping(value = "/random/{max}", produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String showRandomWithMax(@PathVariable int max){
+        int randomNumber = random.nextInt(max) + 1;
+        return "Użytkownik podał wartość max: " + max + ". Wylosowano liczbę: " + randomNumber;
+    }
+*/
     @GetMapping("/show-random")
     @ResponseBody
     public String showRandom(){
-        Random random = new Random();
+
         int rnd = random.nextInt(100);
         return "Wylosowano liczbę: " + rnd;
     }
